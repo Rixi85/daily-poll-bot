5d014e68e295281765f74b1dbb61e1b2dec80913
Rixi85 <morta.cg@gmail.com>
2025-05-06 17:39:28 +0300

    Update daily_poll_bot.py

--- daily_poll_bot.py
+++ daily_poll_bot.py
@@ -47,7 +47,7 @@ def main():
 
     scheduler.add_job(
         lambda: app.create_task(send_daily_poll(ContextTypes.DEFAULT_TYPE(bot=Bot(token=TOKEN)))), 
-        'cron', hour=17, minute=36, timezone=saudi_time
+        'cron', hour=17, minute=41, timezone=saudi_time
     )
 
     scheduler.start()
